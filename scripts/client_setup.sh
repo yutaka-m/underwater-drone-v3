d04493c00de75be01e85e3fe73858f4d3575ad4b
yutaka-matsuzawa <alumican2000@gmail.com>
2020-11-26 21:54:39 +0900

    fix

--- scripts/client_setup.sh
+++ scripts/client_setup.sh
@@ -27,3 +27,19 @@ sudo systemctl status hostapd
 sudo cp ./etc/netplan/50-cloud-init.yaml /etc/netplan/
 sudo netplan apply
 
+# https://github.com/chrippa/ds4drv
+# wget https://github.com/chrippa/ds4drv/blob/master/ds4drv.conf
+sudo pip3 install ds4drv
+wget https://raw.githubusercontent.com/chrippa/ds4drv/master/udev/50-ds4drv.rules
+sudo cp 50-ds4drv.rules /etc/udev/rules.d/
+sudo udevadm control --reload-rules
+sudo udevadm trigger
+ds4drv
+sudo vi /etc/rc.local
+sudo apt install python3 
+apt install python3-pip
+cp scripts/clientJoy.service /lib/systemd/system/
+systemctl status clientJoy
+systemctl enable clientJoy
+systemctl start clientJoy
+
